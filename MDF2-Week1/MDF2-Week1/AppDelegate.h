7ef9189afec994be28aa428711d191b5fc902e97
Jeremy Fox <atljeremy@me.com>
2012-11-21 07:59:53 -0500

    Initial commit

---
+++ MDF2-Week1/MDF2-Week1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MDF2-Week1
+//
+//  Created by Jeremy Fox on 11/20/12.
+//  Copyright (c) 2012 Jeremy Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MDF2-Week1/MDF2-Week1/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  MDF2-Week1
+//
+//  Created by Jeremy Fox on 11/20/12.
+//  Copyright (c) 2012 Jeremy Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
